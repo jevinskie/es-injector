a97755306529f576ab80d957c4510822cfc19c78
Jevin Sweval <jevinsweval@gmail.com>
2022-11-27 09:29:59 -0500

    dumb

--- lib/inject.cpp
+++ lib/inject.cpp
@@ -3,38 +3,111 @@
 #include <EndpointSecurity/EndpointSecurity.h>
 #include <bsm/libbsm.h>
 #include <cassert>
+#include <cstdint>
 #include <dispatch/dispatch.h>
+#include <filesystem>
+#include <mach-o/dyld_images.h>
 #include <mach/mach.h>
 #include <set>
+#include <span>
 
 #include <fmt/format.h>
-#include <frida-gum.h>
 
-extern "C" GUM_API void gum_darwin_enumerate_symbols(mach_port_t task, const gchar *module_name,
-                                                     GumFoundSymbolFunc func, gpointer user_data);
+constexpr uint32_t PAGE_SZ = 4096;
 
-__attribute__((constructor)) void inject_init() {
-    gum_init_embedded();
+struct image_info {
+    uint64_t base;
+    uint64_t size;
+    uint64_t slide;
+    std::filesystem::path path;
+    uint8_t uuid[16];
+};
+
+static void mach_check(kern_return_t kr, const std::string &msg) {
+    if (kr != KERN_SUCCESS) {
+        fmt::print(stderr, "Mach error: '{:s}' retval: {:d} description: '{:s}'\n", msg, kr,
+                   mach_error_string(kr));
+        exit(-1);
+    }
 }
 
-__attribute__((destructor)) void inject_fini() {
-    gum_deinit_embedded();
+// behavior:
+// roundup_pow2_mul(16, 16) = 16
+// roundup_pow2_mul(17, 16) = 32
+template <typename U> static constexpr U roundup_pow2_mul(U num, size_t pow2_mul) {
+    const U mask = static_cast<U>(pow2_mul) - 1;
+    return (num + mask) & ~mask;
+}
+
+static std::vector<uint8_t> read_target(task_t target_task, uint64_t target_addr, uint64_t sz) {
+    std::vector<uint8_t> res;
+    res.resize(sz);
+    vm_size_t vm_sz = sz;
+    const auto kr   = vm_read_overwrite(target_task, (vm_address_t)target_addr, sz,
+                                        (vm_address_t)res.data(), &vm_sz);
+    mach_check(kr, "vm_read_overwrite");
+    assert(vm_sz == sz);
+    return res;
+}
+
+std::string read_cstr_target(task_t target_task, uint64_t target_addr) {
+    std::vector<uint8_t> buf;
+    do {
+        const auto end_addr =
+            target_addr % PAGE_SZ ? roundup_pow2_mul(target_addr, PAGE_SZ) : target_addr + PAGE_SZ;
+        const auto smol_buf = read_target(target_task, target_addr, end_addr - target_addr);
+        buf.insert(buf.end(), smol_buf.cbegin(), smol_buf.cend());
+        target_addr = end_addr;
+    } while (std::find(buf.cbegin(), buf.cend(), '\0') == buf.cend());
+    return {(char *)buf.data()};
+}
+
+std::vector<image_info> get_dyld_image_infos(task_t target_task) {
+    std::vector<image_info> res;
+    task_dyld_info_data_t dyld_info;
+    mach_msg_type_number_t cnt = TASK_DYLD_INFO_COUNT;
+    mach_check(task_info(target_task, TASK_DYLD_INFO, (task_info_t)&dyld_info, &cnt),
+               "task_info dyld info");
+    assert(dyld_info.all_image_info_format == TASK_DYLD_ALL_IMAGE_INFO_64);
+    const auto all_info_buf =
+        read_target(target_task, dyld_info.all_image_info_addr, dyld_info.all_image_info_size);
+    const dyld_all_image_infos *all_img_infos = (dyld_all_image_infos *)all_info_buf.data();
+
+    if (!all_img_infos->infoArray) {
+        return res;
+    }
+
+    const auto dyld_base       = (uint64_t)all_img_infos->dyldImageLoadAddress;
+    const auto dyld_macho_segs = read_macho_segs_target(target_task, dyld_base);
+
+    res.emplace_back(image_info{.base   = dyld_base,
+                                .size   = get_text_size(dyld_macho_segs),
+                                .slide  = dyld_base - get_text_base(dyld_macho_segs),
+                                .path   = read_cstr_target(target_task, all_img_infos->dyldPath),
+                                .uuid   = {},
+                                .is_jit = false});
+
+    const auto infos_buf = read_target(target_task, all_img_infos->infoArray,
+                                       all_img_infos->infoArrayCount * sizeof(dyld_image_info));
+    const auto img_infos = std::span<const dyld_image_info>{(dyld_image_info *)infos_buf.data(),
+                                                            all_img_infos->infoArrayCount};
+    for (const auto &img_info : img_infos) {
+        const auto img_base   = (uint64_t)img_info.imageLoadAddress;
+        const auto macho_segs = read_macho_segs_target(target_task, img_base);
+        res.emplace_back(image_info{.base   = img_base,
+                                    .size   = get_text_size(macho_segs),
+                                    .slide  = img_base - get_text_base(macho_segs),
+                                    .path   = read_cstr_target(target_task, img_info.imageFilePath),
+                                    .uuid   = {},
+                                    .is_jit = false});
+    }
+
+    std::sort(res.begin(), res.end());
+
+    return res;
 }
 
 static void inject_stack(const task_t task, const std::vector<std::string> &injected_env_vars) {}
-
-struct sym_finder_ctx {
-    uint64_t amfi_check_dyld_policy_self_addr;
-};
-
-static gboolean sym_cb(const GumSymbolDetails *details, gpointer user_data) {
-    auto ctx = (sym_finder_ctx *)user_data;
-    if (std::string(details->name) == "_amfi_check_dyld_policy_self") {
-        ctx->amfi_check_dyld_policy_self_addr = details->address;
-        return FALSE;
-    }
-    return TRUE;
-}
 
 static void patch_dyld(const task_t task) {
     sym_finder_ctx ctx{};
@@ -45,9 +118,8 @@ static void patch_dyld(const task_t task) {
 static void inject(const audit_token_t token, const std::vector<std::string> &injected_env_vars) {
     const auto pid = audit_token_to_pid(token);
     assert(pid);
-    task_t task       = MACH_PORT_NULL;
-    const auto kr_tfp = task_for_pid(mach_task_self(), pid, &task);
-    assert(kr_tfp == KERN_SUCCESS);
+    task_t task = MACH_PORT_NULL;
+    mach_check(task_for_pid(mach_task_self(), pid, &task), "tfp");
     patch_dyld(task);
     inject_stack(task, injected_env_vars);
 }
