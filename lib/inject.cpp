220d3f7534bb6a1512324bfe64da2570d9380f10
Jevin Sweval <jevinsweval@gmail.com>
2022-11-28 13:39:20 -0500

    wip

--- lib/inject.cpp
+++ lib/inject.cpp
@@ -2,11 +2,14 @@
 
 #include <EndpointSecurity/EndpointSecurity.h>
 #include <bsm/libbsm.h>
+#undef NDEBUG
 #include <cassert>
 #include <cstdint>
 #include <dispatch/dispatch.h>
 #include <filesystem>
 #include <mach-o/dyld_images.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
 #include <mach/mach.h>
 #include <set>
 #include <span>
@@ -14,14 +17,6 @@
 #include <fmt/format.h>
 
 constexpr uint32_t PAGE_SZ = 4096;
-
-struct image_info {
-    uint64_t base;
-    uint64_t size;
-    uint64_t slide;
-    std::filesystem::path path;
-    uint8_t uuid[16];
-};
 
 static void mach_check(kern_return_t kr, const std::string &msg) {
     if (kr != KERN_SUCCESS) {
@@ -39,80 +34,139 @@ template <typename U> static constexpr U roundup_pow2_mul(U num, size_t pow2_mul
     return (num + mask) & ~mask;
 }
 
-static std::vector<uint8_t> read_target(task_t target_task, uint64_t target_addr, uint64_t sz) {
+static std::vector<uint8_t> read_target(const task_t task, uint64_t addr, uint64_t sz) {
     std::vector<uint8_t> res;
     res.resize(sz);
     vm_size_t vm_sz = sz;
-    const auto kr   = vm_read_overwrite(target_task, (vm_address_t)target_addr, sz,
-                                        (vm_address_t)res.data(), &vm_sz);
+    const auto kr =
+        vm_read_overwrite(task, (vm_address_t)addr, sz, (vm_address_t)res.data(), &vm_sz);
     mach_check(kr, "vm_read_overwrite");
     assert(vm_sz == sz);
     return res;
 }
 
-std::string read_cstr_target(task_t target_task, uint64_t target_addr) {
+static void write_target(const task_t task, uint64_t addr, std::span<const uint8_t> buf) {
+    const auto kr = vm_write(task, (vm_address_t)addr, (vm_offset_t)buf.data(),
+                             (mach_msg_type_number_t)buf.size_bytes());
+    mach_check(kr, "vm_write");
+}
+
+static std::string read_cstr_target(const task_t task, uint64_t addr) {
     std::vector<uint8_t> buf;
     do {
-        const auto end_addr =
-            target_addr % PAGE_SZ ? roundup_pow2_mul(target_addr, PAGE_SZ) : target_addr + PAGE_SZ;
-        const auto smol_buf = read_target(target_task, target_addr, end_addr - target_addr);
+        const auto end_addr = addr % PAGE_SZ ? roundup_pow2_mul(addr, PAGE_SZ) : addr + PAGE_SZ;
+        const auto smol_buf = read_target(task, addr, end_addr - addr);
         buf.insert(buf.end(), smol_buf.cbegin(), smol_buf.cend());
-        target_addr = end_addr;
+        addr = end_addr;
     } while (std::find(buf.cbegin(), buf.cend(), '\0') == buf.cend());
     return {(char *)buf.data()};
 }
 
-std::vector<image_info> get_dyld_image_infos(task_t target_task) {
-    std::vector<image_info> res;
+static uint64_t get_dyld_base(const task_t task) {
     task_dyld_info_data_t dyld_info;
     mach_msg_type_number_t cnt = TASK_DYLD_INFO_COUNT;
-    mach_check(task_info(target_task, TASK_DYLD_INFO, (task_info_t)&dyld_info, &cnt),
+    mach_check(task_info(task, TASK_DYLD_INFO, (task_info_t)&dyld_info, &cnt),
                "task_info dyld info");
     assert(dyld_info.all_image_info_format == TASK_DYLD_ALL_IMAGE_INFO_64);
     const auto all_info_buf =
-        read_target(target_task, dyld_info.all_image_info_addr, dyld_info.all_image_info_size);
+        read_target(task, dyld_info.all_image_info_addr, dyld_info.all_image_info_size);
     const dyld_all_image_infos *all_img_infos = (dyld_all_image_infos *)all_info_buf.data();
-
-    if (!all_img_infos->infoArray) {
-        return res;
-    }
-
-    const auto dyld_base       = (uint64_t)all_img_infos->dyldImageLoadAddress;
-    const auto dyld_macho_segs = read_macho_segs_target(target_task, dyld_base);
-
-    res.emplace_back(image_info{.base   = dyld_base,
-                                .size   = get_text_size(dyld_macho_segs),
-                                .slide  = dyld_base - get_text_base(dyld_macho_segs),
-                                .path   = read_cstr_target(target_task, all_img_infos->dyldPath),
-                                .uuid   = {},
-                                .is_jit = false});
-
-    const auto infos_buf = read_target(target_task, all_img_infos->infoArray,
-                                       all_img_infos->infoArrayCount * sizeof(dyld_image_info));
-    const auto img_infos = std::span<const dyld_image_info>{(dyld_image_info *)infos_buf.data(),
-                                                            all_img_infos->infoArrayCount};
-    for (const auto &img_info : img_infos) {
-        const auto img_base   = (uint64_t)img_info.imageLoadAddress;
-        const auto macho_segs = read_macho_segs_target(target_task, img_base);
-        res.emplace_back(image_info{.base   = img_base,
-                                    .size   = get_text_size(macho_segs),
-                                    .slide  = img_base - get_text_base(macho_segs),
-                                    .path   = read_cstr_target(target_task, img_info.imageFilePath),
-                                    .uuid   = {},
-                                    .is_jit = false});
-    }
-
-    std::sort(res.begin(), res.end());
-
-    return res;
+    assert(all_img_infos->version >= 10); // when amfi_check_dyld_policy_self was added
+    return (uint64_t)all_img_infos->dyldImageLoadAddress;
 }
 
-static void inject_stack(const task_t task, const std::vector<std::string> &injected_env_vars) {}
+static bool is_arm64(const task_t task, const uint64_t dyld_base) {
+    const auto hdr_buf = read_target(task, dyld_base, sizeof(mach_header_64));
+    const auto hdr     = (mach_header_64 *)hdr_buf.data();
+    assert(hdr->magic == MH_MAGIC_64);
+    assert(hdr->cputype == CPU_TYPE_X86_64 || hdr->cputype == CPU_TYPE_ARM64);
+    return hdr->cputype == CPU_TYPE_ARM64;
+}
+
+static uint64_t get_pc(const task_t task, const thread_t thread) {
+#ifdef __arm64__
+    mach_msg_type_number_t gpr_cnt = ARM_THREAD_STATE64_COUNT;
+    arm_thread_state64_t gpr_state;
+    const auto kr_thread_get_gpr =
+        thread_get_state(thread, ARM_THREAD_STATE64, (thread_state_t)&gpr_state, &gpr_cnt);
+    mach_check(kr_thread_get_gpr, "thread_get_state pc");
+    return arm_thread_state64_get_pc(gpr_state);
+#elif defined(__x86_64__)
+    mach_msg_type_number_t gpr_cnt = x86_THREAD_STATE64_COUNT;
+    x86_thread_state64_t gpr_state;
+    const auto kr_thread_get_gpr =
+        thread_get_state(thread, x86_THREAD_STATE64, (thread_state_t)&gpr_state, &gpr_cnt);
+    mach_check(kr_thread_get_gpr, "thread_get_state pc");
+    return gpr_state.__rip;
+#else
+#error bad arch
+#endif
+}
+
+static uint64_t get_sp(const task_t task, const thread_t thread) {
+#ifdef __arm64__
+    mach_msg_type_number_t gpr_cnt = ARM_THREAD_STATE64_COUNT;
+    arm_thread_state64_t gpr_state;
+    const auto kr_thread_get_gpr =
+        thread_get_state(thread, ARM_THREAD_STATE64, (thread_state_t)&gpr_state, &gpr_cnt);
+    mach_check(kr_thread_get_gpr, "thread_get_state sp");
+    return arm_thread_state64_get_sp(gpr_state);
+#elif defined(__x86_64__)
+    mach_msg_type_number_t gpr_cnt = x86_THREAD_STATE64_COUNT;
+    x86_thread_state64_t gpr_state;
+    const auto kr_thread_get_gpr =
+        thread_get_state(thread, x86_THREAD_STATE64, (thread_state_t)&gpr_state, &gpr_cnt);
+    mach_check(kr_thread_get_gpr, "thread_get_state sp");
+    return gpr_state.__rsp;
+#else
+#error bad arch
+#endif
+}
+
+static uint64_t get_amfi_check_dyld_policy_self_addr(const task_t task, const uint64_t dyld_base) {
+    const auto hdr_buf = read_target(task, dyld_base, sizeof(mach_header_64));
+    const auto hdr     = (mach_header_64 *)hdr_buf.data();
+    assert(hdr->magic == MH_MAGIC_64);
+    const auto cmd_buf   = read_target(task, dyld_base + sizeof(mach_header_64), hdr->sizeofcmds);
+    const auto end_of_lc = (load_command *)(cmd_buf.data() + hdr->sizeofcmds);
+    for (auto lc = (load_command *)cmd_buf.data(); lc < end_of_lc;
+         lc      = (load_command *)((uint8_t *)lc + lc->cmdsize)) {
+        if (lc->cmd != LC_SYMTAB) {
+            continue;
+        }
+        const auto symtab = (symtab_command *)lc;
+        const auto symbuf =
+            read_target(task, dyld_base + symtab->symoff, symtab->nsyms * sizeof(nlist_64));
+        const auto syms   = (nlist_64 *)symbuf.data();
+        const auto strbuf = read_target(task, dyld_base + symtab->stroff, symtab->strsize);
+        const auto strs   = strbuf.data();
+        for (uint32_t i = 0; i < symtab->nsyms; ++i) {
+            const auto sym  = syms[i];
+            const auto name = std::string((char *)&strs[sym.n_un.n_strx]);
+            fmt::print("name: {:s}\n", name);
+            if (name == "_amfi_check_dyld_policy_self") {
+                return dyld_base + sym.n_value;
+            }
+        }
+        assert(!"amfi_check_dyld_policy_self not found");
+    }
+    assert(!"symtab not found");
+}
+
+static void inject_stack(const task_t task, const thread_t thread,
+                         const std::vector<std::string> &injected_env_vars) {}
 
 static void patch_dyld(const task_t task) {
-    sym_finder_ctx ctx{};
-    gum_darwin_enumerate_symbols(task, nullptr, sym_cb, (gpointer)&ctx);
-    fmt::print("amfi_check_dyld_policy_self: {:p}\n", (void *)ctx.amfi_check_dyld_policy_self_addr);
+    const auto dyld_base = get_dyld_base(task);
+    const auto arm64     = is_arm64(task, dyld_base);
+#ifdef __arm64__
+    assert(arm64);
+#else
+    assert(!arm64);
+#endif
+    uint64_t amfi_check_dyld_policy_self_addr =
+        get_amfi_check_dyld_policy_self_addr(task, dyld_base);
+    fmt::print("amfi_check_dyld_policy_self: {:p}\n", (void *)amfi_check_dyld_policy_self_addr);
 }
 
 static void inject(const audit_token_t token, const std::vector<std::string> &injected_env_vars) {
@@ -120,8 +174,17 @@ static void inject(const audit_token_t token, const std::vector<std::string> &in
     assert(pid);
     task_t task = MACH_PORT_NULL;
     mach_check(task_for_pid(mach_task_self(), pid, &task), "tfp");
+    thread_act_array_t thread_list;
+    mach_msg_type_number_t num_threads;
+    const auto kr_threads = task_threads(task, &thread_list, &num_threads);
+    mach_check(kr_threads, "task_threads");
+    assert(num_threads == 1);
+    thread_t thread       = thread_list[0];
+    const auto kr_dealloc = vm_deallocate(mach_task_self(), (vm_address_t)thread_list,
+                                          sizeof(thread_act_t) * num_threads);
+    mach_check(kr_dealloc, "vm_deallocate");
     patch_dyld(task);
-    inject_stack(task, injected_env_vars);
+    inject_stack(task, thread, injected_env_vars);
 }
 
 static void es_cb(es_client_t *client, const es_message_t *message,
